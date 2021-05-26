e817e9966fd232a2a602f4bb84360b104c7ebb29
Darren Behan <darrenbehan@hotmail.com>
2021-05-26 14:38:18 +1000

    conditional statement added to home page to display cards if matches are available, map method added to cards to assign match data to each card on display

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,25 +1,27 @@
 import React, { useContext } from 'react';
-import './index.css';
 import DataAreaContext from '../../utils/DataAreaContext';
+import './index.css';
 import { CardDeck, Card } from 'react-bootstrap';
 
 function Cards() {
-  const {} = useContext(DataAreaContext);
+  const { allMatches } = useContext(DataAreaContext);
 
   return (
     <CardDeck>
-      <Card>
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to additional
-            content.{' '}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
+      {allMatches.map(match => 
+        <Card>
+          <Card.Body>
+            <Card.Title> { match.competitionName } </Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated: { match.updatedAt }</small>
+          </Card.Footer>
+        </Card>
+      )}
     </CardDeck>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import DataAreaContext from '../utils/DataAreaContext';
 import Header from "../components/Header";
 import Cards from '../components/Cards';
-import Container from '@material-ui/core/Container';
+import { Container } from 'react-bootstrap';
 
 function Home() {
+  const { allMatches } = useContext(DataAreaContext);
 
   return (
     <>
     <Header />
-    <Container component="main" maxWidth="xs">
-      <Cards />
-    </Container>
+    <Container>
+      {allMatches.length ? (
+        <Cards />
+      ) : (
+        <h3 style={{ textAlign: "center" }}>No Results to Display</h3>
+      )}
+      </Container>
     </>
   );
 }

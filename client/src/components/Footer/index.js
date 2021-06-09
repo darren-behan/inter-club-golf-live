8f40fd626b07824051d3c1de960db8bb9b07921e
Darren Behan <darrenbehan@hotmail.com>
2021-06-09 14:50:46 +1000

    added conditional check to display x if logged in and y if not

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './index.css';
+import DataAreaContext from '../../utils/DataAreaContext';
 import { Container, Navbar } from 'react-bootstrap';
 
 function Footer() {
+  const { isAuthenticated } = useContext(DataAreaContext);
 
   return (
     <Navbar expand="lg" variant="light" bg="light" fixed="bottom">
       <Container>
-        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        {!isAuthenticated ? (
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        ) : (
+          <div>Logged In</div>
+        )}
       </Container>
     </Navbar>
   );

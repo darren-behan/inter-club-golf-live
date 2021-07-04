f2dcbe29d7269fac15338be1602f4d64d0b81271
Darren Behan <darrenbehan@hotmail.com>
2021-07-04 10:55:20 +1000

    updating firebase functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,47 @@
 module.exports = {
-  "extends": [
-    "airbnb",
-    "plugin:prettier/recommended",
-    "prettier/react"
-  ],
   "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "jest": true,
-    "node": true
+      "browser": true,
+      "commonjs": true,
+      "es6": true
+  },
+  "extends": ["eslint:recommended"],
+  "parserOptions": {
+      "sourceType": "module"
   },
   "rules": {
-    "jsx-a11y/href-no-hash": ["off"],
-    "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
-    "max-len": [
-      "warn",
-      {
-        "code": 120,
-        "tabWidth": 2,
-        "comments": 120,
-        "ignoreComments": false,
-        "ignoreTrailingComments": true,
-        "ignoreUrls": true,
-        "ignoreStrings": true,
-        "ignoreTemplateLiterals": true,
-        "ignoreRegExpLiterals": true
-      }
-    ]
+      "indent": ["error", 4],
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      "no-console": "off"
   }
+  // "extends": [
+  //   "airbnb",
+  //   "plugin:prettier/recommended",
+  //   "prettier"
+  // ],
+  // "env": {
+  //   "browser": true,
+  //   "commonjs": true,
+  //   "es6": true,
+  //   "jest": true,
+  //   "node": true
+  // },
+  // "rules": {
+  //   "jsx-a11y/href-no-hash": ["off"],
+  //   "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
+  //   "max-len": [
+  //     "warn",
+  //     {
+  //       "code": 120,
+  //       "tabWidth": 2,
+  //       "comments": 120,
+  //       "ignoreComments": false,
+  //       "ignoreTrailingComments": true,
+  //       "ignoreUrls": true,
+  //       "ignoreStrings": true,
+  //       "ignoreTemplateLiterals": true,
+  //       "ignoreRegExpLiterals": true
+  //     }
+  //   ]
+  // }
 }

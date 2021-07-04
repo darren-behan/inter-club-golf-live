fbcf3ccee4fb0728bd84467be74b80c8b6cdea00
Darren Behan <darrenbehan@hotmail.com>
2021-07-04 15:51:23 +1000

    update to app for deployment

--- client/src/utils/Lib.js
+++ client/src/utils/Lib.js
@@ -1,16 +1,18 @@
 export default {
-  // Logs in a user
   capitalize: function(string) {
+    if (string === "" || string === null || string === undefined) {
+      return;
+    }
     return string.charAt(0).toUpperCase() + string.slice(1);
   },
-  removeByAttr: function(arr, attr, value){
+  removeByAttr: function(arr, attr, value) {
     var i = arr.length;
-    while(i--){
-       if( arr[i] 
-           && arr[i].hasOwnProperty(attr) 
-           && (arguments.length > 2 && arr[i][attr] === value ) ){ 
-           arr.splice(i,1);
-       }
+    while (i--) {
+      if (arr[i] &&
+        arr[i].hasOwnProperty(attr) &&
+        (arguments.length > 2 && arr[i][attr] === value)) {
+        arr.splice(i, 1);
+      }
     }
     return arr;
   }

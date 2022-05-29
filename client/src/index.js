488ce3f9c5098d576da6c36018f6da51281c8aa6
Darren Behan <darrenbehan@hotmail.com>
2022-05-29 15:35:00 +1000

    fixing firebase initialization error

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TagManager from 'react-gtm-module';
-const { initializeApp } = require('firebase/app');
+import firebase from 'firebase/compat/app';
 const { initializeAppCheck, ReCaptchaV3Provider } = require('firebase/app-check');
 
 const tagManagerArgs = {
@@ -14,25 +14,26 @@ const tagManagerArgs = {
 
 TagManager.initialize(tagManagerArgs);
 
-const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-});
-
 if (window.location.hostname === 'localhost') {
   window.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 }
 
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider(process.env.REACT_APP_APP_CHECK),
-  isTokenAutoRefreshEnabled: true,
-});
+const appCheck = initializeAppCheck(
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  }),
+  {
+    provider: new ReCaptchaV3Provider(process.env.REACT_APP_APP_CHECK),
+    isTokenAutoRefreshEnabled: true,
+  },
+);
 
 ReactDOM.render(
   <React.StrictMode>
